00da33450137c52df1ec62c6a8e4621c6306c1f3
Palldanerar <103989677+Palldanerar@users.noreply.github.com>
2023-06-02 08:43:47 +0300

    version 1.1

--- src/db.ts
+++ src/db.ts
@@ -4,13 +4,111 @@ const quotes: IQuote[] = [
   {
     id: 1,
     ru_lang: {
-      title: 'Чем умнее человек, тем легче он признает себя дураком.',
+      title: 'Умный человек решает проблему. Мудрый человек избегает ее.',
       author: 'Альберт Эйнштейн',
     },
     en_lang: {
-      title:
-        'The smarter a person is, the easier he recognizes himself as a fool.',
+      title: 'A clever person solves a problem. A wise person avoids it.',
       author: 'Albert Einstein',
+    },
+  },
+  {
+    id: 2,
+    ru_lang: {
+      title: 'Надо любить жизнь больше, чем смысл жизни.',
+      author: 'Фёдор Достоевский',
+    },
+    en_lang: {
+      title: 'Love life more than the meaning of it.',
+      author: 'Fyodor Dostoyevsky',
+    },
+  },
+  {
+    id: 3,
+    ru_lang: {
+      title: 'Учись из прошлого, живи настоящим, надейся на будущее.',
+      author: 'Альберт Эйнштейн',
+    },
+    en_lang: {
+      title: 'Learn from yesterday, live for today, hope for tomorrow.',
+      author: 'Albert Einstein',
+    },
+  },
+  {
+    id: 4,
+    ru_lang: {
+      title: 'Истинный признак интеллекта не знания, а воображение.',
+      author: 'Альберт Эйнштейн',
+    },
+    en_lang: {
+      title: 'The true sign of intelligence is not knowledge but imagination.',
+      author: 'Albert Einstein',
+    },
+  },
+  {
+    id: 5,
+    ru_lang: {
+      title: 'Вас никогда не постигнет неудача, пока вы не перестанете стараться.',
+      author: 'Альберт Эйнштейн',
+    },
+    en_lang: {
+      title: 'You never fail until you stop trying.',
+      author: 'Albert Einstein',
+    },
+  },
+  {
+    id: 6,
+    ru_lang: {
+      title: 'Ваше время ограничено, так что не теряйте его, проживая чужую жизнь.',
+      author: 'Стив Джобс',
+    },
+    en_lang: {
+      title: 'Your time is limited, so don’t waste it living someone else’s life.',
+      author: 'Steve Jobs',
+    },
+  },
+  {
+    id: 7,
+    ru_lang: {
+      title: 'Выживает не самый сильный и не самый умный, а тот, кто лучше всех приспосабливается к изменениям.',
+      author: 'Чарльз Дарвин',
+    },
+    en_lang: {
+      title: 'It is not the strongest of the species that survives, nor the most intelligent, but the one most responsive to change.',
+      author: 'Charles Darwin',
+    },
+  },
+  {
+    id: 8,
+    ru_lang: {
+      title: 'Никогда не сожалей о том, что заставляет тебя улыбаться.',
+      author: 'Марк Твен',
+    },
+    en_lang: {
+      title: 'Never regret anything that made you smile.',
+      author: 'Mark Twain'
+    },
+  },
+  {
+    id: 9,
+    ru_lang: {
+      title: 'Жизнь это 10% того, что с нами происходит, и 90% того, как мы на это реагируем.',
+      author: 'Чарльз Свиндолл',
+    },
+    en_lang: {
+      title: 'Life is 10% what happens to us and 90% how we react to it.',
+      author: 'Charles Swindoll'
+    },
+  },
+  {
+    id: 10,
+    ru_lang: {
+      title: 'Нельзя быть слишком хорошо одетым или слишком хорошо образованным.',
+      author: 'Оскар Уайльд',
+    },
+    en_lang: {
+      title: 'You can never be overdressed or overeducated.',
+      author: 'Oscar Wilde'
     },
   },
 ];
